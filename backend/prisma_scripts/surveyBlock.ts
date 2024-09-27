19bcb6f3f154affdaa9a81bd4f6558ac386f0c8f
Daniel Gavidia <dgavidia1@gmail.com>
2024-09-26 20:52:19 -0400

    .

--- backend/prisma_scripts/surveyBlock.ts
+++ backend/prisma_scripts/surveyBlock.ts
@@ -1,18 +1,5 @@
 import prisma from "../prisma/client";
 
-// Retrieves all survey blocks
-// export const getSurveyBlocks = async ()
-
-// Deletes survey block
-export const deleteSurveyBlock = async (id: string) => {
-    const surveyBlock = await prisma.surveyBlock.delete({
-        where: {
-            id: id,
-        },
-    });
-    console.log(surveyBlock);
-};
-
 // Updates default survey block answer
 export const updateSurveyBlockAnswer = async (id: string, answer: string) => {
     const surveyBlock = await prisma.surveyBlock.update({
